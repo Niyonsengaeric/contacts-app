2f798b654f6e16979ccaac0fc07b68d113e84afc
Niyonsengaeric <niyeric11@gmail.com>
2020-08-27 17:10:54 +0200

    control the add button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 
 class App extends Component {
   state = {
     contacts: [],
+    screen:'list'
   };
   componentDidMount() {
     ContactsAPI.getAll()
@@ -26,10 +28,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
+        {this.state.screen === 'list' && (<ListContacts
           contacts={this.state.contacts}
           onDeleteContact={this.deleteContact}
-        />
+          onNavigate={()=>{this.setState(()=>({screen:'create'}))}}
+        />)}
+        {this.state.screen === 'create' && (<CreateContact/>)}
       </div>
     );
   }
